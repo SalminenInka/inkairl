397e0634fa11d073365fe5327be608107f070b7e
inka <inka_salminen@hotmail.com>
2022-07-28 17:08:09 +0300

    Pretty good

--- dist/app.ts
+++ dist/app.ts
@@ -1,45 +1,45 @@
-const { readFile, writeFile } = require("fs/promises")
-const fs = require("fs")
-const express = require('express');
+import { readFile, writeFile } from "fs/promises";
+import fs = require("fs");
+import express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
-const { error } = require("console");
-const { v4: uuidv4 } = require('uuid');
+import bodyParser = require("body-parser");
+import { error } from "console";
+import { v4 as uuidv4 } from 'uuid';
 const app = express();
-////////////////////////////////////////////////
+const environ = require('dotenv').config()
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
-/////////////////////////////////////////////////
-const fileName = './db.json';
+//check if file exists and create one if needed
+const fileName = process.env.FILENAME;
 const fileExists = fs.existsSync(fileName);
 console.log(`${fileName} exists:`, fileExists);
 if (!fileExists) {
   console.log("Creating the file")
   fs.writeFileSync(fileName, JSON.stringify({}));
 }
-/////////////////////////////////////////////////
+//create new user with post()
 router.post('/users/new', async (req,res) => {
   try {
-    const contents = await readFile(fileName);
+    const contents = await readFile(fileName, 'utf-8');
     const database = JSON.parse(contents);
     database[uuidv4()] = req.body;
     const string = JSON.stringify(database)
     await writeFile(fileName, string);
-    res.send('New user data created.');
+    res.send(Object.keys(database)[Object.keys(database).length-1]);
   } catch (err) {
     res.status(500).send('Failed to create new user.');
   }
 });
-//////////////////////////////////////////////////
-
-router.delete('/users/delete/:id', async (req, res) => {
+//delete user data with user/id
+router.delete('/users/:id', async (req, res) => {
   try {
-    const contents = await readFile(fileName);
+    const contents = await readFile(fileName, 'utf-8');
     const database = JSON.parse(contents);
     const check = database.hasOwnProperty(req.params.id)
-    if (check === false) {
-      res.send('No such data');
+    if (check == false) {
+      res.status(404).send('No such data');
     } else {
       delete database[req.params.id];
       const dataString = JSON.stringify(database)
@@ -50,14 +50,13 @@ router.delete('/users/delete/:id', async (req, res) => {
     res.status(500).send('Failed to delete user data.');
   }
 });
-////////////////////////////////////////////////
-router.put('/users/update/:id', async (req, res) => {
+//update user data with put()
+router.put('/users/:id', async (req, res) => {
   try {
-    const contents = await readFile(fileName);
+    const contents = await readFile(fileName, 'utf-8');
     const database = JSON.parse(contents);
-    const check = database.hasOwnProperty(req.params.id);
-    if (check === false) {
-      res.send('No such data');
+    if (database.hasOwnProperty(req.params.id) == false) {
+      res.status(404).send('No such data');
     } else {
       database[req.params.id] = req.body;
       const dataString = JSON.stringify(database)
@@ -68,14 +67,13 @@ router.put('/users/update/:id', async (req, res) => {
     res.status(500).send('Failed to update user data.');
   }
 });
-////////////////////////////////////////////////
+//display user data for user/specific id
 router.get('/users/:id', async (req, res) => {
   try {
-    const contents = await readFile(fileName);
+    const contents = await readFile(fileName, 'utf-8');
     const database = JSON.parse(contents);
-    const check = database.hasOwnProperty(req.params.id);
-    if (check === false) {
-      res.send('No such data');
+    if (database.hasOwnProperty(req.params.id) == false) {
+      res.status(404).send('No such data');
     } else {
       res.send({...database[req.params.id]});
     }
@@ -84,20 +82,19 @@ router.get('/users/:id', async (req, res) => {
   }
 
 })
-
+//dispaly all user data, all users
 router.get('/users', async (req, res) => {
   try {
-    const contents = await readFile(fileName);
+    const contents = await readFile(fileName, 'utf-8');
     const database = JSON.parse(contents);
-    if (contents === {}) {
+    if (Object.keys(database).length === 0) {
       res.status(404).send('Database seems to be empty.')
     } else {
-      res.send({...database});
+      res.send(database);
     }
   } catch (err) {
     res.status(500).send('Failed to retrieve user data.');
   }
 })
 
-////////////////////////////////////////////////
-app.listen(666);
+app.listen(process.env.PORT);
---
+++ index.js
@@ -0,0 +1,100 @@
+const { readFile, writeFile } = require("fs/promises")
+const fs = require("fs")
+const express = require('express');
+const router = express.Router();
+const bodyParser = require("body-parser");
+const { error } = require("console");
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+const environ = require('dotenv').config()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/", router);
+//check if file exists and create one if needed
+const fileName = process.env.FILENAME;
+const fileExists = fs.existsSync(fileName);
+console.log(`${fileName} exists:`, fileExists);
+if (!fileExists) {
+  console.log("Creating the file")
+  fs.writeFileSync(fileName, JSON.stringify({}));
+}
+//create new user with post()
+router.post('/users/new', async (req,res) => {
+  try {
+    const contents = await readFile(fileName);
+    const database = JSON.parse(contents);
+    database[uuidv4()] = req.body;
+    const string = JSON.stringify(database)
+    await writeFile(fileName, string);
+    res.send(Object.keys(database)[Object.keys(database).length-1]);
+  } catch (err) {
+    res.status(500).send('Failed to create new user.');
+  }
+});
+//delete user data with user/id
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const contents = await readFile(fileName);
+    const database = JSON.parse(contents);
+    const check = database.hasOwnProperty(req.params.id)
+    if (check == false) {
+      res.status(404).send('No such data');
+    } else {
+      delete database[req.params.id];
+      const dataString = JSON.stringify(database)
+      await writeFile(fileName, dataString);
+      res.send('One entry deleted from the database.');
+    }
+  } catch (err) {
+    res.status(500).send('Failed to delete user data.');
+  }
+});
+//update user data with put()
+router.put('/users/:id', async (req, res) => {
+  try {
+    const contents = await readFile(fileName);
+    const database = JSON.parse(contents);
+    if (database.hasOwnProperty(req.params.id) == false) {
+      res.status(404).send('No such data');
+    } else {
+      database[req.params.id] = req.body;
+      const dataString = JSON.stringify(database)
+      await writeFile(fileName, dataString);
+      res.send('User updated.');
+    }
+  } catch (err) {
+    res.status(500).send('Failed to update user data.');
+  }
+});
+//display user data for user/specific id
+router.get('/users/:id', async (req, res) => {
+  try {
+    const contents = await readFile(fileName);
+    const database = JSON.parse(contents);
+    if (database.hasOwnProperty(req.params.id) == false) {
+      res.status(404).send('No such data');
+    } else {
+      res.send({...database[req.params.id]});
+    }
+  } catch (err) {
+    res.status(500).send('Failed to retrieve user data.');
+  }
+
+})
+//dispaly all user data, all users
+router.get('/users', async (req, res) => {
+  try {
+    const contents = await readFile(fileName);
+    const database = JSON.parse(contents);
+    if (Object.keys(database).length === 0) {
+      res.status(404).send('Database seems to be empty.')
+    } else {
+      res.send(database);
+    }
+  } catch (err) {
+    res.status(500).send('Failed to retrieve user data.');
+  }
+})
+
+app.listen(process.env.PORT);
